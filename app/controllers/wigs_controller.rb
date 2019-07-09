fd743e91343358484580ec7063276e25f0386dcc
Stan Levavasseur <stan10l@hotmail.com>
2019-07-09 16:29:56 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/wigs_controller.rb
+++ app/controllers/wigs_controller.rb
@@ -1,2 +1,20 @@
 class WigsController < ApplicationController
+  def new
+    @wig = Wig.new
+  end
+
+  def create
+    @Wig = Wig.new(wig_params)
+    if @wig.save
+      redirect_to wig_path(@wig)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def wig_params
+    params.require(:wig).permit(:title, :photo, :description, :price, :color, :size)
+  end
 end
