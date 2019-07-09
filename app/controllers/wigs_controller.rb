961b12b13bc0e419d0c7e36b0c85f542498faa17
Luis-Luca de Haas <luislucadehaas@gmail.com>
2019-07-09 17:29:55 +0200

    finished search

--- app/controllers/wigs_controller.rb
+++ app/controllers/wigs_controller.rb
@@ -1,8 +1,24 @@
 class WigsController < ApplicationController
 
   def index
-    @wigs = Wig.all
+
+    if params[:search].present? && params["search"]["query"] == ""
+      @wigs = Wig.all
+    elsif params[:search].present?
+      sql_query = " \
+        wigs.title @@ :search \
+        OR wigs.description @@ :search \
+        OR wigs.color @@ :search \
+      "
+      @wigs = Wig.where(sql_query, search: "%#{params["search"]["query"]}%")
+    else
+      @wigs = Wig.all
+    end
   end
 
 
+private
+  def wig_params
+    params.require(:wig).permit(:title, :description, :price, :color, :size)
+  end
 end
--- db/migrate/20190709130340_add_image_to_wigs.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToWigs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :wigs, :image, :string
-  end
-end
