679f4ac9eff4e5ff891cc8e15f3d02a302d1990c
Max Brendle <maxbrendle@icloud.com>
2019-07-10 11:41:31 +0200

    optimised design, dynamic picture link missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ max = User.create(email:'max@test.com', password:'testword')
 louis = User.create(email:'louis@test.com', password:'testword')
 stan = User.create(email:'stan@test.com', password:'testword')
 Wig.create(user_id: max.id, description:'This is the most piraty wig of all piraty wigs. Yo will feel like a badass pirate if you rock this pirating wig.  Happy pirating!', price: 40, color:'black', size:'m', title: 'Jacky Sparowsi')
-Wig.create(user_id: louis.id, description:'Make yourself the diva you always wanted to be. With this natural feeling look you will rock any party! ', price: 5000, color:'blonde', size:'m', title: 'Marys Madness')
-Wig.create(user_id: louis.id, description:'Make yourself the diva you always wanted to be. With this natural feeling look you will rock any party! ', price: 5000, color:'blonde', size:'m', title: 'Marys Madness')
+Wig.create(user_id: louis.id, description:'Make yourself the diva you always wanted to be. With this natural feeling look you will rock any party!', price: 5000, color:'blonde', size:'m', title: 'Marys Madness')
+Wig.create(user_id: louis.id, description:'How often do experts say you should listen to Bob Marley? Reggae-larly. No ', price: 5000, color:'blonde', size:'m', title: 'Marys Madness')
 
