6e25b208593e3a9616e4b3138b079c33637c9346
Max Brendle <maxbrendle@icloud.com>
2019-07-11 17:49:03 +0200

    message is on show and migration is generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_143800) do
+ActiveRecord::Schema.define(version: 2019_07_11_151957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_143800) do
     t.bigint "wig_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "message"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["wig_id"], name: "index_bookings_on_wig_id"
   end
